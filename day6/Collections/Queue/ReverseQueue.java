4afd2979d8a3891cb3ce2bb08ada925604748e52
jawahar2003 <jawahar6343@gmail.com>
2025-04-19 17:30:00 +0530

    Initial commit

---
+++ day6/Collections/Queue/ReverseQueue.java
@@ -0,0 +1,27 @@
+import java.util.Queue;
+import java.util.LinkedList;
+
+
+public class ReverseQueue {
+   public static void main(String[] args) {
+       Queue<Integer> queue = new LinkedList<>();
+       queue.add(1);
+       queue.add(2);
+       queue.add(3);
+       queue.add(4);
+       reverse(queue);
+       System.out.println("Original Queue: " + queue);
+   }
+
+
+   static void reverse(Queue<Integer> queue){
+       int first = 0;
+       if(!queue.isEmpty()){
+           first = queue.remove();
+           reverse(queue);
+       }else{
+           return;
+       }
+       queue.add(first);
+      
+   }
